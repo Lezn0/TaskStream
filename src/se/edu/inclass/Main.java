915f1e14f587448481e201cb056f8a3eb834e44a
Lezn0 <yuxinwu1234@gmail.com>
2020-09-18 17:05:10 +0800

    Add methods using streams

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -20,7 +20,10 @@ public class Main {
         printDeadlines(tasksData);
 
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
+        //printDataUsingStreams(tasksData);
+        printDeadlinesUsingStreams(tasksData);
 
+        System.out.println("total number of deadlines using streams:" + countDeadlinesUsingStreams(tasksData));
     }
 
     private static int countDeadlines(ArrayList<Task> tasksData) {
@@ -38,7 +41,26 @@ public class Main {
             System.out.println(t);
         }
     }
+    public static void printDataUsingStreams (ArrayList<Task> tasksData){
+        System.out.println("printing data using streams");
+        tasksData.stream().forEach(System.out::println);
+    }
 
+    public static void printDeadlinesUsingStreams (ArrayList<Task> tasksData){
+        System.out.println("printing deadlines using streams");
+        tasksData.stream()
+                .filter((t)-> t instanceof Deadline)
+                .forEach(System.out::println);
+    }
+
+    public static int countDeadlinesUsingStreams(ArrayList<Task> tasksData){
+        System.out.println("calculating count using streams");
+        int count;
+        count = (int) tasksData.stream()
+                .filter((t)-> t instanceof Deadline)
+                .count();
+        return count;
+    }
     public static void printDeadlines(ArrayList<Task> tasksData) {
         for (Task t : tasksData) {
             if (t instanceof Deadline) {
